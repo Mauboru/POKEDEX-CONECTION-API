fcaa4f61301b86a4c5d5963636f7973b9282ebd3
Josue Henrique <josue21henrique@gmail.com>
2023-12-07 16:06:52 -0300

    inserindo metas

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Projeto Final da Matéria de WEB I
 
 Site de criação de pókedex usando a API https://pokeapi.co/
+
+# Inserir
+- Selecionar Cards e manter selecionados
+- Guardar seleção em um array
+- Criar nova pókedex com os selecionados
+- se selecionar ele terá opção capturar (adiciona a alguma pókedex)
+- criar página invisivel que só aparece ao clicar na pókedex
+- ela listará os pókemons apenas por nome ou icon
+- poderá selecionar a pókedex e deletar
--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
   const apiUrl = 'https://pokeapi.co/api/v2/pokemon';
   const limit = 200;
   let offset = 0;
+  let selectedPokemons = [];
 
   function getPokemons() {
     $.get({
@@ -47,6 +48,52 @@ $(document).ready(function () {
 
     return tipo;
   }
+
+  function createPokemonCard(pokemonData) {
+    return `
+      <div class="col-md-3 mb-3">
+        <div class="card">
+          <img src="${pokemonData.sprites.versions['generation-v']['black-white'].animated.front_default}" class="card-img" alt="${pokemonData.name}" style="width: ${'120px'}; height: ${'150px'};">
+          <div class="card-body">
+            <h5 class="card-title">${pokemonData.name}</h5>
+            <p class="card-text"><strong>Tipo:</strong> ${getPokemonTipo(pokemonData.types)}</p>
+            <button class="btn btn-primary add-to-pokedex" data-name="${pokemonData.name}">Adicionar à Pokédex</button>
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  function createPokedex() {
+    const pokedexName = 'Pokédex-001'; // Nome padrão
+    console.log(`Pokédex criada: ${pokedexName}`);
+    console.log('Pokémons selecionados:', selectedPokemons);
+  }
+
+  function updateSelectedPokemonContainer() {
+    $('#selectedPokemonContainer').empty();
+    selectedPokemons.forEach(pokemon => {
+      let card = createPokemonCard(pokemon);
+      $('#selectedPokemonContainer').append(card);
+    });
+  }
+
+  $(document).on('click', '.add-to-pokedex', function () {
+    const pokemonName = $(this).data('name');
+    const pokemon = selectedPokemons.find(p => p.name === pokemonName);
+
+    if (!pokemon) {
+      selectedPokemons.push({
+        name: pokemonName,
+        types: $(this).closest('.card-body').find('.card-text span').text()
+      });
+      updateSelectedPokemonContainer();
+    }
+  });
+
+  $('#createPokedexBtn').click(function () {
+    createPokedex();
+  });
 
   getPokemons();
 });
